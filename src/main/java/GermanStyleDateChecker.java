853a41315cccce93775010d414f38ae580dfa51d
Simon Holzapfel <simon.holzapfel@gmx.at>
2020-06-02 08:55:54 +0200

    giving my best...

--- src/main/java/GermanStyleDateChecker.java
+++ src/main/java/GermanStyleDateChecker.java
@@ -3,6 +3,8 @@ import java.util.regex.Pattern;
 
 public class GermanStyleDateChecker {
     public static Matcher getMatcher(String dateString) {
-        return null;
+        String regex = "(3(0|1))|([0-2][1-9])(\\.|-| )(1[0-2])|(0?[1-9])(\\.|-| )(1\\n\\n\\n)|((20)?\\n\\n)";
+        Pattern pattern = Pattern.compile(regex);
+        return pattern.matcher(dateString);
     }
 }
