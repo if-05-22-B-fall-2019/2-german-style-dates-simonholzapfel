178f2fef3fadceb958c9ba2e29febea64b3cd05e
Simon Holzapfel <simon.holzapfel@gmx.at>
2020-06-02 13:23:44 +0200

    finished

--- src/main/java/GermanStyleDateChecker.java
+++ src/main/java/GermanStyleDateChecker.java
@@ -3,7 +3,7 @@ import java.util.regex.Pattern;
 
 public class GermanStyleDateChecker {
     public static Matcher getMatcher(String dateString) {
-        String regex = "(3(0|1))|([0-2][1-9])(\\.|-| )(1[0-2])|(0?[1-9])(\\.|-| )(1\\n\\n\\n)|((20)?\\n\\n)";
+        String regex = "\\A((3([01]))|([0-2]?[1-9]))(\\.|-| )((1[0-2])|(0?[1-9]))(\\.|-| )((19\\d\\d)|((20)?\\d\\d))\\Z";
         Pattern pattern = Pattern.compile(regex);
         return pattern.matcher(dateString);
     }
